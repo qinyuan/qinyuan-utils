2f74a42fe37908cafe0f96704ab917df69497709
qinyuan <qinyuan15@sina.com>
2015-06-19 13:38:30 +0800

    update

--- src/main/java/com/qinyuan15/utils/mvc/controller/BaseController.java
+++ src/main/java/com/qinyuan15/utils/mvc/controller/BaseController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 public class BaseController {
+    protected final static String BLANK_PAGE = "blank";
 
     @Autowired
     protected HttpServletRequest request;
