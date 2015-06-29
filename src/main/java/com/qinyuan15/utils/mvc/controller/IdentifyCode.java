961c17acfc5c540de5fc90fb98839d28385467ab
qinyuan <qinyuan15@sina.com>
2015-06-29 15:31:06 +0800

    update

--- src/main/java/com/qinyuan15/utils/mvc/controller/IdentifyCode.java
+++ src/main/java/com/qinyuan15/utils/mvc/controller/IdentifyCode.java
@@ -16,8 +16,16 @@ public class IdentifyCode {
     private final static int HEIGHT = 40;
 
     // the chars that can be shown in identify code
-    private final static char mapTable[] = {'0', '1', '2', '3', '4', '5', '6',
-            '7', '8', '9'};
+    private final static char mapTable[] = {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g',
+            'h', 'i', 'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G',
+            'H', 'I', 'J', 'K', 'L', 'M', 'N',
+            'O', 'P', 'Q', 'R', 'X', 'T',
+            'U', 'V', 'W', 'X', 'Y', 'Z'};
 
     private IdentifyCode() {
     }
--- src/main/java/com/qinyuan15/utils/mvc/controller/IdentityCodeController.java
+++ src/main/java/com/qinyuan15/utils/mvc/controller/IdentityCodeController.java
@@ -4,14 +4,13 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;import java.lang.Exception;import java.lang.Override;import java.lang.String;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Controller to output identity code
  * Created by qinyuan on 15-6-28.
  */
 public class IdentityCodeController extends AbstractController {
-
     @Override
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setContentType("image/jpeg");
