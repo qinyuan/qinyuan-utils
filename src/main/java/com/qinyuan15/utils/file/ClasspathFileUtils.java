5c26421ae7d76050962de4fd1338dcc95d98e530
qinyuan <qinyuan15@sina.com>
2015-07-15 16:01:05 +0800

    update

--- src/main/java/com/qinyuan15/utils/file/ClasspathFileUtils.java
+++ src/main/java/com/qinyuan15/utils/file/ClasspathFileUtils.java
@@ -1,6 +1,9 @@
 package com.qinyuan15.utils.file;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 
 /**
@@ -11,5 +14,14 @@ public class ClasspathFileUtils {
     public static File getFile(String relativePath) {
         URL url = ClasspathFileUtils.class.getClassLoader().getResource(relativePath);
         return url == null ? null : new File(url.getFile());
+    }
+
+    public static InputStream getInputStream(String relativePath) {
+        File file = getFile(relativePath);
+        try {
+            return new FileInputStream(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
