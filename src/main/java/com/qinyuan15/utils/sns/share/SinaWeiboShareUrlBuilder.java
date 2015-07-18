c3599364017c31104ba6aac188559d509d037d06
qinyuan <qinyuan15@sina.com>
2015-07-18 11:31:52 +0800

    update

--- src/main/java/com/qinyuan15/utils/sns/share/SinaWeiboShareUrlBuilder.java
+++ src/main/java/com/qinyuan15/utils/sns/share/SinaWeiboShareUrlBuilder.java
@@ -10,10 +10,10 @@ import java.util.Map;
  * Class to build share url of sina weibo
  * Created by qinyuan on 15-7-16.
  */
-public class XinaWeiboShareUrlBuilder extends AbstractShareUrlBuilder {
+public class SinaWeiboShareUrlBuilder extends AbstractShareUrlBuilder {
     private final Map<String, String> params = new HashMap<>();
 
-    public XinaWeiboShareUrlBuilder(String targetUrl, String title, List<String> pictures) {
+    public SinaWeiboShareUrlBuilder(String targetUrl, String title, List<String> pictures) {
         params.put("title", title);
         params.put("url", targetUrl);
         params.put("pic", Joiner.on("||").join(pictures));
--- src/test/java/com/qinyuan15/utils/sns/share/SinaWeiboShareUrlBuilderTest.java
+++ src/test/java/com/qinyuan15/utils/sns/share/SinaWeiboShareUrlBuilderTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class XinaWeiboShareUrlBuilderTest {
+public class SinaWeiboShareUrlBuilderTest {
     @Test
     public void testBuild() throws Exception {
         String targetUrl = "http://www.sogou.com";
@@ -16,7 +16,7 @@ public class XinaWeiboShareUrlBuilderTest {
                 "https://www.baidu.com/img/bdlogo.png",
                 "http://www.sogou.com/images/logo/new/search400x150.png"
         );
-        String url = new XinaWeiboShareUrlBuilder(targetUrl, title, images).build();
+        String url = new SinaWeiboShareUrlBuilder(targetUrl, title, images).build();
         assertThat(url).isEqualTo("http://service.weibo.com/share/share.php?title=HelloWorld&pic=https%3A%2F%2Fwww.baidu.com%2Fimg%2Fbdlogo.png%7C%7Chttp%3A%2F%2Fwww.sogou.com%2Fimages%2Flogo%2Fnew%2Fsearch400x150.png&url=http%3A%2F%2Fwww.sogou.com");
     }
 }
