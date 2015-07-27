e9c13b47a8bc7b2cbbcc4a64aba7a39e09ffb394
qinyuan <qinyuan15@sina.com>
2015-07-27 21:58:25 +0800

    update

--- src/main/java/com/qinyuan15/utils/ip/DefaultIpLocationQuerier.java
+++ src/main/java/com/qinyuan15/utils/ip/DefaultIpLocationQuerier.java
@@ -1,8 +1,32 @@
 package com.qinyuan15.utils.ip;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DefaultIpLocationQuerier extends ComposableIpLocationQuerier {
 
     public DefaultIpLocationQuerier() {
-        super(new TaobaoIpLocationQuerier(), new BaiduIpLocationQuerier(), new SogouIpLocationQuerier());
+        super(getQueries());
+    }
+
+    private static List<IpLocationQuerier> getQueries() {
+        List<IpLocationQuerier> queriers = new ArrayList<>();
+        queriers.add(new DatabaseIpLocationQuerier());
+
+        IpLocationSaver ipLocationSaver = new DatabaseIpLocationSaver();
+
+        NetworkIpLocationQuerier querier = new TaobaoIpLocationQuerier();
+        querier.setIpLocationSaver(ipLocationSaver);
+        queriers.add(querier);
+
+        querier = new BaiduIpLocationQuerier();
+        querier.setIpLocationSaver(ipLocationSaver);
+        queriers.add(querier);
+
+        querier = new SogouIpLocationQuerier();
+        querier.setIpLocationSaver(ipLocationSaver);
+        queriers.add(querier);
+
+        return queriers;
     }
 }
