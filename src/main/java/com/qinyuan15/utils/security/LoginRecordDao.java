a2c486b1d64424ca85e75609091ff3f69a852fa4
qinyuan <qinyuan15@sina.com>
2015-08-01 12:11:48 +0800

    update

--- src/main/java/com/qinyuan15/utils/security/LoginRecordDao.java
+++ src/main/java/com/qinyuan15/utils/security/LoginRecordDao.java
@@ -1,15 +1,25 @@
 package com.qinyuan15.utils.security;
 
 import com.qinyuan15.utils.DateUtils;
+import com.qinyuan15.utils.IntegerUtils;
 import com.qinyuan15.utils.hibernate.HibernateListBuilder;
 import com.qinyuan15.utils.hibernate.HibernateUtils;
 import com.qinyuan15.utils.ip.DefaultIpLocationQuerier;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LoginRecordDao {
     public List<LoginRecord> getInstances() {
         return new HibernateListBuilder().build(LoginRecord.class);
+    }
+
+    public List<LoginRecord> getInstances(Integer userId) {
+        if (!IntegerUtils.isPositive(userId)) {
+            return new ArrayList<>();
+        }
+
+        return new HibernateListBuilder().addEqualFilter("userId", userId).build(LoginRecord.class);
     }
 
     private Integer add(Integer userId, String ip, String location, String loginTime) {
